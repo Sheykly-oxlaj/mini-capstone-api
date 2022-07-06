68fc284fee4220e21ef8ee836cf00c9c525a0c6c
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-06 10:05:09 -0700

    adds method to the model (product) and adds the model method to the show action in the controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,26 @@
 class Product < ApplicationRecord
+  
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def is_discounted?
+    if price < 10
+      return true     #Return is unnecesary
+    else 
+      return false
+    end 
+  end 
+
+  def tax
+    #9% rate
+    tax = price * 0.09
+  end 
+
+
+  def total 
+    total = tax + price
+    "The total price, including taxes is $#{total}"
+  end 
+
 end
