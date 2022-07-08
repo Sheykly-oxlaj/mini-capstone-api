13deb042d7ed864102aa2cdddfa0d21e829d2c37
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-08 10:49:43 -0700

    adds validations and happy/sad paths

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,8 +26,12 @@ class ProductsController < ApplicationController
       price: params["price"],
       image_url: params["image_url"],
       description: params["description"])
-    @product.save 
+    
+    if @product.save 
     render template: "products/show"
+    else 
+      render json: {errors: product.errors_full_messages}, status 418
+    end 
   end 
 
   def update
@@ -39,8 +43,11 @@ class ProductsController < ApplicationController
       @product.image_url = params["image_url"] || @product.image_url
       @product.description = params["description"] || @product.description
 
-    @product.save
+    if @product.save 
     render template: "products/show"
+    else 
+      render json: {errors: product.errors_full_messages}, status 418
+    end 
   end 
 
   def destroy
