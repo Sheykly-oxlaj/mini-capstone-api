6f0013be39518bcc0c54907f51276f84d263f0ad
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-12 10:28:58 -0700

    cleans up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,18 @@
 Rails.application.routes.draw do
-  
+  #Product controllers
   # get "/products", controller: "products", action: "index"
   get "/products" => "products#index"
-
-  # get "/products/:id", controller: "products", action: "show"
   get "/products/:id" => "products#show"
-
-  # post "/products", controller: "products", action: "create"
   post "/products" => "products#create"
-
   patch "/products/:id" => "products#update"
-
   delete "/products/:id" => "products#destroy"
 
 
-  # get "/products", controller: "products", action: "index"
+  #Supplier Controllers
   get "/suppliers" => "suppliers#index"
-
-  # get "/suppliers/:id", controller: "suppliers", action: "show"
   get "/suppliers/:id" => "suppliers#show"
-
-  # post "/suppliers", controller: "suppliers", action: "create"
   post "/suppliers" => "suppliers#create"
-
   patch "/suppliers/:id" => "suppliers#update"
-
   delete "/suppliers/:id" => "suppliers#destroy"
 
 end
