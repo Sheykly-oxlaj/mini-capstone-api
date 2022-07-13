9c5efc7b7dbe3d22a74be3a3b75aabf4a43fdc2a
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-13 12:03:47 -0700

    updates orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,32 @@
 class OrdersController < ApplicationController
   
   def index
-    orders = Order.all
+    orders = current_user.orders
     render json: orders.as_json
   end
 
-  def show 
-    order = Order.find_by(id: params[id:])
+  def show
+    order = current_user.orders.find_by(id: params[:id])
     render json: order.as_json
-  end 
+  end
   
   def create
-  order = Order.new(
-    user_id: params["user_id"],
-    product_id: params["product_id"],
-    quantity: params["quantity"],
-    subtotal: params["subtotal"],
-    tax: params["tax"],
-    total: params["total"],
-  )
-  order.save
-  render json: { "message": "Order Created Succesfully, look below for order summary", order: order.as_json }
+    product = Product.find_by(id: params[:product_id])
 
-  end 
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
+    order = Order.new(
+      user_id: current_user.id,
+      product_id: params["product_id"],
+      quantity: params["quantity"],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
+    )
+
+    order.save
+    render json: order.as_json
+  end
 end
