62a17f8eb0f78cefbb8fb9b8ac30a0e686889539
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-15 18:41:36 -0700

    makes edits to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,61 +1,61 @@
 class ProductsController < ApplicationController
-
   before_action :authenticate_admin, except: [:index, :show]
 
-  def index 
-    @products = Product.all
-    render template: "products/index" 
-  end 
+  def index
+    if params[:category]
+      category = Category.find_by(name: params[:category])
+      @products = category.products
+    end
+    render :index
+  end
+
   # def all_products_method
   #   products = Product.all
   #   render json: products.as_json
-  # end 
-  def show 
+  # end
+  def show
     @product = Product.find_by(id: params[:id])
-    render template: "products/show"
-  end 
+    render :show
+  end
 
-  # def product_method 
+  # def product_method
   #   product = Product.find_by(id: params["id"])
   #   render json: product.as_json
-  # end 
+  # end
 
-  def create 
-    @product = Product.create(
-      name: params["name"],
-      price: params["price"],
-      # image_url: params["image_url"],
-      description: params["description"],
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      # image_url: params[:image_url],
+      description: params[:description],
     )
-    
-    if @product.save 
-    render template: "products/show"
-    else 
-      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
-    end 
-  end 
+    if @product.save #happy path
+      render :show
+    else # sad path
+      render json: { errors: @product.errors.full_messages },
+             status: 418
+    end
+  end
 
   def update
-    product_id = params["id"]
-    @product = Product.find_by(id: product_id)
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    # @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
 
-      @product.name = params["name"]  || @product.name 
-      @product.price = params["price"] || @product.price
-      # @product.image_url = params["image_url"] || @product.image_url
-      @product.description = params["description"] || @product.description
-
-      if @product.save 
-        render template: "products/show"
-      else 
-        render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
-      end 
-  end 
+    if @product.save #happy path
+      render :show
+    else # sad path
+      render json: { errors: @product.errors.full_messages },
+             status: 418
+    end
+  end
 
   def destroy
-    product_id = params["id"]
-    product = Product.find_by(id: product_id)
+    product = Product.find_by(id: params[:id])
     product.destroy
-    render json: { message: "Product succesfully deleted, this product has beecome unavailable"}
-  end 
-
+    render json: { message: "Product succesfully deleted, this product has beecome unavailable" }
+  end
 end
