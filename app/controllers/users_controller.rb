3e3f9ef4d53a05693079f91e57fb59cb5d45ceb3
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-15 18:43:22 -0700

    makes edits to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,15 @@
 class UsersController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      jwt = JWT.encode(
-        {
-          user_id: user.id, # the data to encode
-          exp: 24.hours.from_now.to_i # the expiration time
-        },
-        Rails.application.credentials.fetch(:secret_key_base), # the secret key
-        "HS256" # the encryption algorithm
-      )
-      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
     else
-      render json: {}, status: :unauthorized
+      render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
 end
