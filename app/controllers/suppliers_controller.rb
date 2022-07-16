d4adcc4b1df9e4f37731e0365473ddb7033a4298
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-07-15 18:44:20 -0700

    formats code correctly

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,33 +1,32 @@
 class SuppliersController < ApplicationController
-
-  def index 
+  def index
     suppliers = Supplier.all
     render json: suppliers.as_json
-  end 
+  end
 
-  def show 
+  def show
     supplier = Supplier.find_by(id: params["id"])
     render json: supplier.as_json
-  end 
+  end
 
-  def create 
+  def create
     supplier = Suplplier.create(
       name: params["name"],
       email: params["email"],
-      phone_number: params["phone_number"])
-  end 
+      phone_number: params["phone_number"],
+    )
+  end
 
   def update
-      supplier = Supplier.find_by(id: params["id"])
-      supplier.name = params["name"]  || supplier.name 
-      supplier.email = params["email"] || supplier.email
-      supplier.phone_number = params["phone_number"] || supplier.phone_number
-  end 
+    supplier = Supplier.find_by(id: params["id"])
+    supplier.name = params["name"] || supplier.name
+    supplier.email = params["email"] || supplier.email
+    supplier.phone_number = params["phone_number"] || supplier.phone_number
+  end
 
   def destroy
     supplier = Supplier.find_by(id: params["id"])
     supplier.destroy
-    render json: { message: "supplier succesfully deleted, this supplier has beecome unavailable"}
-  end 
-
+    render json: { message: "supplier succesfully deleted, this supplier has beecome unavailable" }
+  end
 end
